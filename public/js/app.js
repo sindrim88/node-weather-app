7bf263e91126ac207eb73d58033cdfd994b44f07
sindrim88 <sindrim88@gmail.com>
2021-11-12 11:22:50 +0000

    app.js cleanup

--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,3 @@
-console.log('Client side javascript file is loaded');
-
-
 /// testing something to fetch
 // fetch('http://puzzle.mead.io/puzzle').then((response) => {
 //     response.json().then((data) => {
